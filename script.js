be6d6945ee4015ee5567b15c2faca120aa10dd02
wjdgmltjr <ldy8617@gmail.com>
2025-07-05 20:18:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ document.querySelector("#ask-button").addEventListener("click", async () => {
   const data = await res.json();
   responseDiv.innerText = data.answer;
 });
+document.querySelector("#ask-button").addEventListener("click", async () => {
+  const question = document.querySelector("#question-input").value;
+  const responseBox = document.querySelector("#response");
+  responseBox.innerText = "답변을 불러오는 중이에요...";
+
+  const res = await fetch("https://us-central1-your-project-id.cloudfunctions.net/ask", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ prompt: question })
+  });
+
+  const data = await res.json();
+  responseBox.innerText = data.answer;
+});
